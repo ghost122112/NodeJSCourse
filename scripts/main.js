418aaffa7d6914403c0e340ea1d818b293f5939a
ghost122112 <112318377+ghost122112@users.noreply.github.com>
2022-10-06 12:45:01 +0500

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-
 var http = require('http');
 
 http.createServer(function (req, res) {
@@ -7,21 +6,32 @@ http.createServer(function (req, res) {
 }).listen(8080);
 
 
+exports.myDateTime = () => {
+    return Date();
+}
 
 
+var http = require('http');
+var dt = require('../scripts');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime);
+    res.end();
+}).listen(8080);
+
+// #1 step: Create an upload form.
+// #2 step: Parse the uploaded file.
+// #3 step: Save the file.
 
 
+var http = require('http');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+}).listen(8080);
