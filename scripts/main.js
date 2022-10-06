5754154f2fa208492074370eb3c15dd1daa1cc5a
ghost122112 <112318377+ghost122112@users.noreply.github.com>
2022-10-06 12:06:13 +0500

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 
 http.createServer(function (req, res) {
@@ -5,12 +6,27 @@ http.createServer(function (req, res) {
   res.end('Hello World!');
 }).listen(8080);
 
+*/
+
+const timer = () => {
+    let d = new Date();
+    let time = d.toLocaleTimeString();
+    console.log(time);
+    return document.getElementById("timer").innerHTML = time;
+}
+
+const interval = setInterval(timer, 1000);
 
 
 
+const myObj = Object({
+    firstname: "John",
+    lastname: "Doe",
+    age: 19
+});
 
-
-
+document.getElementById("obj_data").innerText = myObj["firstname"];
+document.getElementById("obj_data").innerText = myObj["lastname"];
 
 
 
